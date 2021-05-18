447651cb5ef25c301f0525a213109f4ce96a7b6a
Patrick R Sears <patrick_sears@med.unc.edu>
2021-05-18 13:51:38 -0400

    Initial commit - v 0.5-a1

---
+++ m.py
@@ -0,0 +1,62 @@
+
+
+
+
+from m0_c00_config import *
+from m1_basic_control import *
+from m2_plate_6well import *
+from m3_mctd_8fov import *
+from m4_center_fovs import *
+
+
+
+c00 = Cc00_config()
+c00.load()
+c00.print_loaded_config()
+
+
+
+
+
+c1 = Cfov8(1)
+c2 = Cfov8(2)
+c3 = Cfov8(3)
+c4 = Cfov8(4)
+c5 = Cfov8(5)
+c6 = Cfov8(6)
+
+def load_all_zeros():
+  c1.load_zero()
+  c2.load_zero()
+  c3.load_zero()
+  c4.load_zero()
+  c5.load_zero()
+  c6.load_zero()
+
+def save_all_zeros():
+  c1.save_zero()
+  c2.save_zero()
+  c3.save_zero()
+  c4.save_zero()
+  c5.save_zero()
+  c6.save_zero()
+
+wp = Cplate_6well()   # wp:  well plate
+#################################
+def wp_sefidu(fidu_name):
+  status, x, y = c00.get_fidu(fidu_name)
+  if status != 0:  return
+  wp.sefidu(x,y)
+#################################
+def wp_load_inspos():
+  wp.load_inspos( c00.fname_plate_6well_inspos_lis )
+#################################
+
+# general culture
+gc = Cmculture()
+
+
+
+gf = Cgofov();
+
+
---
+++ m99_sim_serial.py
@@ -0,0 +1,50 @@
+
+
+import platform
+import socket
+
+
+############################################
+print('platform.system(): ', platform.system())
+hostname = socket.gethostname()
+print('hostname: ', hostname)
+############################################
+
+
+##################################################################
+class C_sim_serial:
+  ###
+  def __init__(self):
+    self.x = 0
+  ###
+  def write(self, s):
+    print("> ", s)
+  ###
+  def readline(self):
+    return bytes( "101,102,103\r\n".encode() )
+  ###
+# module serial
+##################################################################
+
+
+############################################
+if hostname == 'shiva2':
+  #
+  import serial
+  #
+  # Starts serial port when module is loaded.
+  # spo:  serial port
+  spo = serial.Serial(
+    port='COM5', baudrate=9600, bytesize=8,
+    timeout=2, stopbits=serial.STOPBITS_ONE
+    )
+  #
+else:
+  # from m99_sim_serial import *
+  spo = C_sim_serial();
+############################################
+
+
+
+
+
