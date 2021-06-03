262241cfcc64aacfd4f069e29bcaafbaea43130d
Patrick R Sears - shiva2 <patrick_sears@med.unc.edu>
2021-06-03 15:39:36 -0400

    Make locup get user input and call controller to get current position

--- modules/c_locup.py
+++ modules/c_locup.py
@@ -1,14 +1,23 @@
 
+from m1_basic_control import p
 
 #######################################################
 class c_locup:
   #
   def __init__(self):
-    x0 = None
-    y0 = None
+    self.x0 = None
+    self.y0 = None
+    self.cnum = 1    # the culture number
   #
   def run(self):
-    print("Running.")
+    pline = "Go to culture "+str(self.cnum)
+    pline += " North edge. and hit e[enter]\n"
+    while True:
+      print(pline)
+      uline = input("in<< ")
+      if uline == 'q':  return
+      if uline == 'e':  break
+    p()
 #######################################################
 
 
