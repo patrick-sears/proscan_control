6f0f91d605dec6203290269569c3b295d657b98f
Patrick R Sears <patrick_sears@med.unc.edu>
2023-07-20 13:30:19 -0400

    Change muwp hui, uses "run" or "run.b"
    Or even "run.a".
    The run.b version is the old r4e.run.

--- modules/c_muwp.py
+++ modules/c_muwp.py
@@ -389,58 +389,51 @@ class c_muwp:
       elif uline == 'save plate':  self.save_plate()
       elif uline == 'create lps':
         self.create_locups()
-      elif uline.startswith('run c'):
-        ll = uline.strip().split(' ')
-        if len(ll)== 2:
-          # iw = int(ll[1][2:])-1
-          iws = ll[1][1:]
-          if not iws.isdigit():
-            print("uError.")
-            print("  c not followed by a digit.")
-            continue
-          iw = int(iws)-1
+      elif uline.startswith('run'):
+        run_mode = 'a'
+        if   uline.startswith('run '):    run_mode = 'a'
+        elif uline.startswith('run.a '):  run_mode = 'a'
+        elif uline.startswith('run.b '):  run_mode = 'b'
         else:
+          print("uError.  Unrecognized input.")
+          continue
+        #
+        ll = uline.strip().split(' ')
+        if len(ll) != 2:
           print("Strange uline split length.")
           print("  Need 2 words.")
           print("  Example:  run c2")
           continue
+        if not ll[1][0] == 'c':
+          print("uError.")
+          print("  Missing c.")
+          continue
+        iws = ll[1][1:]
+        if not iws.isdigit():
+          print("uError.")
+          print("  c not followed by a digit.")
+          continue
+        iw = int(iws)-1
         if iw < 0 or iw >= len(self.mlocup):
           print("Entered number is out of range.")
           print("  n_well:      ", self.n_well)
           print("  len(mlocup): ", len(self.mlocup))
           continue
-        self.mlocup[iw].run_pattern()
-      elif uline.startswith('r4e.run c'):
-        ll = uline.strip().split(' ')
-        if len(ll)== 2:
-          # iw = int(ll[1][2:])-1
-          iws = ll[1][1:]
-          if not iws.isdigit():
-            print("uError.")
-            print("  c not followed by a digit.")
-            continue
-          iw = int(iws)-1
-        else:
-          print("Strange uline split length.")
-          print("  Need 2 words.")
-          print("  Example:  run c2")
-          continue
-        if iw < 0 or iw >= len(self.mlocup):
-          print("Entered number is out of range.")
-          print("  n_well:      ", self.n_well)
-          print("  len(mlocup): ", len(self.mlocup))
-          continue
-        self.mlocup[iw].go_edge( 'N' )
-        rv = self.mlocup[iw].get_edges()
-        if rv == 0:
-          # all is ok
-          print("Resetting muwp ins",iw+1,"center.")
-          self.ins_center_x[iw] = self.mlocup[iw].cx
-          self.ins_center_y[iw] = self.mlocup[iw].cy
-        else:
-          print("locup get_edges() didn't return 0.")
-          print("  So not resetting muwp ins center.")
-        self.mlocup[iw].run_pattern()
+        ###
+        if   run_mode == 'a':   self.mlocup[iw].run_pattern()
+        elif run_mode == 'b':
+          self.mlocup[iw].go_edge( 'N' )
+          rv = self.mlocup[iw].get_edges()
+          if rv == 0:
+            # all is ok
+            print("Resetting muwp ins",iw+1,"center.")
+            self.ins_center_x[iw] = self.mlocup[iw].cx
+            self.ins_center_y[iw] = self.mlocup[iw].cy
+          else:
+            print("locup get_edges() didn't return 0.")
+            print("  So not resetting muwp ins center.")
+            print("  Running patter...")
+          self.mlocup[iw].run_pattern()
         #
       elif uline.startswith('setrun lp '):
         ll = uline.strip().split(' ')
