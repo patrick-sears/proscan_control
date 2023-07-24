8c4f51de47a6489a58342852ea4f508d5d992c4c
Patrick R Sears <patrick_sears@med.unc.edu>
2023-07-24 13:00:42 -0400

    Make MW-Win batch files start minimized

--- start_01_in_IDLE.bat
+++ start_01_in_IDLE.bat
@@ -1,4 +1,6 @@
 
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~pdnx0" %* && exit
+
 :: Clicking on this opens the python IDLE
 :: and leaves the cmd window also open.
 
@@ -6,5 +8,7 @@
 :: color f0
 
 python -m idlelib
+
+exit
 
 
--- start_arec.bat
+++ start_arec.bat
@@ -1,5 +1,7 @@
 
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~pdnx0" %* && exit
 
 python -m idlelib -r modules_u/arec.py
 
+exit
 
